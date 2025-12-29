5981d03ad7290bf88e25e9da9c8d7ebeabc212cb
Shobhan1000 <shobhansharma10@gmail.com>
2025-12-29 15:43:11 +0000

    Added feature where button is disables during search

--- src/Weather.jsx
+++ src/Weather.jsx
@@ -5,6 +5,16 @@ import axios from 'axios'
 const Weather = () => {
   const [city, setCity] = useState("");
   const [weather, setWeather] = useState(null);
+
+  const [isButtonDisabled, setButtonDisabled] = useState(false);
+
+  const disableButton = () => {
+        setButtonDisabled(true);
+    }
+
+  const enableButton = () => {
+        setButtonDisabled(false);
+    }
   
   const getWeather = async () => {
     if (!city) {
@@ -13,15 +23,21 @@ const Weather = () => {
     }
 
     try {
+      disableButton();
       const apiKey = import.meta.env.VITE_WEATHER_API_KEY;
       const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
       
       const response = await axios.get(url);
       console.log(response.data);
       setWeather(response.data);
+      enableButton();
     } catch (error) {
       console.error("Error fetching weather data:", error);
+      if (error.response && error.response.status === 404) {
+        alert("City not found. Please check the city name and try again.");
+      }
       setWeather(null);
+      enableButton();
     }
   };
 
@@ -44,7 +60,7 @@ const Weather = () => {
               onChange={(e) => setCity(e.target.value)}
             />
           </label>
-          <button type="submit">Get Weather</button>
+          <button type="submit" disabled={isButtonDisabled}>Get Weather</button>
         </form>
       </div>
       {weather && (
