b1fcf7f1de162af125550edf51285630b28099b1
Shobhan1000 <shobhansharma10@gmail.com>
2025-12-29 15:24:05 +0000

    API integration

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,12 @@
-import { useState } from 'react'
-import './App.css'
+import React from "react";
+import Weather from "./Weather";
 
 function App() {
-  const [name, setName] = useState("");
-
-  function handleChange(e) {
-    setName(e.target.value);
-  }
-
-  function handleSubmit(e) {
-    e.preventDefault();
-    alert(name);
-  }
-
   return (
-    <>
-      <h1>City Weather Dashboard</h1>
-      <div className="card px-4 py-5">
-        <form onSubmit={handleSubmit}>
-          <label>
-            Enter the city name:
-            <input
-              type="text" 
-              value={name}
-              onChange={handleChange}
-            />
-          </label>
-          <input type="submit" value="Submit" />
-        </form>
-      </div>
-      <div className='weatherCard'>
-        {/* Weather information will be displayed here */}
-        <h1>Weather Information</h1>
-      </div>
-    </>
-  )
+    <div className="App">
+      <Weather />
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/Weather.jsx
@@ -0,0 +1,61 @@
+import React, { useState } from "react";
+import './App.css'
+import axios from 'axios'
+
+const Weather = () => {
+  const [city, setCity] = useState("");
+  const [weather, setWeather] = useState(null);
+  
+  const getWeather = async () => {
+    if (!city) {
+        alert("Please enter a city name");
+        return;
+    }
+
+    try {
+      const apiKey = import.meta.env.VITE_WEATHER_API_KEY;
+      const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+      
+      const response = await axios.get(url);
+      console.log(response.data);
+      setWeather(response.data);
+    } catch (error) {
+      console.error("Error fetching weather data:", error);
+      setWeather(null);
+    }
+  };
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    getWeather();
+  }
+
+  return (
+    <>
+      <h1>City Weather Dashboard</h1>
+      <div className="card px-4 py-5">
+        <form onSubmit={handleSubmit}>
+          <label>
+            Enter the city name:
+            <input
+              type="text" 
+              value={city}
+              placeholder="Enter city"
+              onChange={(e) => setCity(e.target.value)}
+            />
+          </label>
+          <button type="submit">Get Weather</button>
+        </form>
+      </div>
+      {weather && (
+        <div>
+          <h2>{weather.name}</h2>
+          <p>Temperature: {weather.main.temp}°C</p>
+          <p>Weather: {weather.weather[0].description}</p>
+        </div>
+      )}
+    </>
+  )
+}
+
+export default Weather
